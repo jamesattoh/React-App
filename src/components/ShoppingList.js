7915b4f2ae8b4dff2914998c86248e432e2a1d06
jamesattoh <koss.attoh27@gmail.com>
2024-12-09 00:49:03 +0100

    using conditions or && on lists with map()

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from '../datas/plantList'
+import '../styles/shoppingList.css'
 
 function ShoppingList() {
     /**
@@ -17,11 +18,17 @@ function ShoppingList() {
 	)
 
     /**
+     * À partir d’une liste de données,  map()   permet de créer une liste de composants React
+     * 
+     * La prop key est indispensable dans les listes de composants
+     * 
      * categories.map((cat) => (<li key={cat}>{cat}</li>)) génère une liste <ul> 
      * avec un élément <li> pour chaque catégorie dans categories, avec lol comme clé
      * *****
      * plantList.map((plant) => (<li key={plant.id}>{plant.name}</li>)) génère une 
      * autre liste <ul> avec un élément <li> pour chaque plante dans plantList, avec plant.id comme clé
+     * 
+     * && précède un élément JSX précise que l'élément ne sera généré que si la condition est respectée.
      */
 	return (
 		<div>
@@ -30,9 +37,12 @@ function ShoppingList() {
 					<li key={lol}>{lol}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name}</li>
+					<li key={plant.id} className='lmj-plant-item'>
+                        {plant.name} 
+                        {plant.isSpecialOffer && <span className='lmj-sales'>Soldes</span>}
+                    </li>
 				))}
 			</ul>
 		</div>
