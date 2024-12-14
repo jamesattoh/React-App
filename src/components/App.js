bbf67bfae74f0eb0562a3eb2aff8d96a32243cdb
jamesattoh <koss.attoh27@gmail.com>
2024-12-15 00:57:49 +0100

    adding comments & some files

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,30 @@
 import Banner from "./Banner";
-import Cart from "./Cart";
-import ShoppingList from "./ShoppingList";
 import logo from "../assets/logo.png"
-import '../styles/banner.css'
+import Cart from "./Cart";
 import Footer from "./Footer";
+import ShoppingList from "./ShoppingList";
+import '../styles/banner.css'
+import '../styles/layout.css'
+
 
 function App() {
   return (
     <div>
+      
       <h2>Hello Ici Dolgen 🔥</h2>
 
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+
 			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
+          <Cart />
+          <ShoppingList />
 			</div>
+
 			<Footer />
+
     </div>
   )
 
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,6 +3,18 @@ import '../styles/cart.css'
 
 function Cart() {
 	const monsteraPrice = 8
+    /**
+     *  on peut créer un state cart . Avec  useState  , nous devons  déclarer
+     *  en même temps une fonction pour mettre à jour ce state ( updateCart  ),
+     *  et lui attribuer une valeur initiale, qui sera ici de 0 :
+     * les crochets  [] ici, ça s'appelle la décomposition,
+     * useState   nous renvoie une paire de valeurs dans un tableau de 2 éléments,
+     *  que nous récupérons dans les variables  cart   et  updateCart 
+     * 
+     * J'aurais tout aussi bien pu faire :
+     * const [coucou, cavabien] = useState(0)
+
+     */
 	const [cart, updateCart] = useState(0)
 	const [isOpen, setIsOpen] = useState(true)
 
